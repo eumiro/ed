249d59f6b4ca91fac616fe542eee9186faf22423
Miroslav Šedivý <eumiro@users.noreply.github.com>
2017-12-25 22:35:55 +0100

    add basic ranges and print command

--- pyed/buffer.py
+++ pyed/buffer.py
@@ -8,6 +8,45 @@ class Buffer:
             self.lines = []
         else:
             self.lines = [str(line) for line in lines]
+        self.cur = len(self.lines)
 
     def __len__(self):
         return len(('\n'.join(self.lines)).encode('utf8'))
+
+    def find_pos(self, raw_pos):
+        raw_lower, comma, raw_upper = raw_pos.partition(',')
+
+        if comma:
+            if raw_lower.isdigit():
+                lower = int(raw_lower)
+            elif raw_lower == '':
+                lower = 1
+            else:
+                raise NotImplementedError(f'invalid lower pos {raw_lower!r}')
+
+            if raw_upper.isdigit():
+                upper = int(raw_upper)
+                self.cur = upper
+            elif raw_upper == '':
+                upper = len(self.lines)
+            else:
+                raise NotImplementedError(f'invalid upper pos {raw_upper!r}')
+        else:
+            if raw_lower.isdigit():
+                lower = int(raw_lower)
+                upper = lower
+            elif raw_lower == '':
+                lower = self.cur
+                upper = self.cur + 1
+            else:
+                raise NotImplementedError(f'invalid lower pos {raw_lower!r}')
+
+        return slice(lower - 1, upper)
+
+    def run(self, cmd):
+        if cmd.endswith('p'):
+            rng = self.find_pos(cmd[:-1])
+            if self.cur:
+                return self.lines[rng]
+            else:
+                return []
--- tests/test_buffer.py
+++ tests/test_buffer.py
@@ -19,3 +19,23 @@ def test_init_invalid():
                           ([1, 'b'], 3)])
 def test_init_ok_len(lines, size):
     assert len(Buffer(lines)) == size
+
+
+@pytest.fixture(scope='function')
+def buffer_five():
+    return Buffer(['one', 'two', 'three', 'four', 'five'])
+
+
+def test_empty_cmd_p():
+    buffer = Buffer()
+    assert buffer.run('p') == []
+
+
+@pytest.mark.parametrize('cmd, res',
+                         [('p', ['five']),
+                          ('1p', ['one']),
+                          (',2p', ['one', 'two']),
+                          ('4,p', ['four', 'five']),
+                          ('1,3p', ['one', 'two', 'three'])])
+def test_five_cmd(cmd, res, buffer_five):
+    assert buffer_five.run(cmd) == res
