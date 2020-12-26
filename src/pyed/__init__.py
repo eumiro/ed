13e6355f1e7dc25583c1b70b43d3115260a5b2a1
Miroslav Šedivý <6774676+eumiro@users.noreply.github.com>
2020-12-26 11:28:05 +0100

    add setup.py

---
+++ src/pyed/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.1.0"
+
+__title__ = "pyed"
+__description__ = "the standard text editor"
+__url__ = "https://github.com/eumiro/pyed/"
+__uri__ = __url__
+__doc__ = __description__ + " <" + __uri__ + ">"
+
+__author__ = "Miroslav Šedivý"
+__email__ = "eumiro@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2020 Miroslav Šedivý"
